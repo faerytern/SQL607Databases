8571dd752566a54fd442f938556040a90fbbfb6c
faerytern/AdmiralAckweed <60916296+faerytern@users.noreply.github.com>
2021-04-08 13:00:32 +1200

    Create DropTables.sql

---
+++ Assignment1/DropTables.sql
@@ -0,0 +1,9 @@
+--In correct order from CreateTable.sql
+DROP TABLE Tickets;
+DROP TABLE TrainSeatsFareprices;
+DROP TABLE FareTypes;
+DROP TABLE TrainRoutes;
+DROP TABLE [Routes];
+DROP TABLE DaysAvailable;
+DROP TABLE TrainList;
+DROP TABLE Passenger;
